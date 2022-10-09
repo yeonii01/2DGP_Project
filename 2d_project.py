411b10ccaaa962f66314114165ac89133b6ecfe7
yeonii01 <adencho1204@naver.com>
2022-10-09 18:50:12 +0900

    handle_event 추가 및 코드 수정

--- 2d_project.py
+++ 2d_project.py
@@ -6,11 +6,21 @@ class Knight:
         self.frame = 0
         self.image = load_image('knight_sprite.png')
     def update(self):
-        self.frame = (self.frame+1) % 12
+        self.frame = (self.frame+1) % 7
     def draw(self):
         self.image.clip_draw(self.frame*80, 303, 80, 80, self.x, self.y)
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 open_canvas()
+
 knight = Knight()
 running = True
 while running:
@@ -21,9 +31,6 @@ while running:
     knight.draw()
     update_canvas()
 
-    delay(0.1)
+    delay(0.2)
 
 close_canvas()
-
-
-
