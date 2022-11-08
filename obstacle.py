ef2daaa39d0779271d3bc8386cb3a31d1a79d382
yeonii01 <adencho1204@naver.com>
2022-11-09 02:41:24 +0900

    장애물 클래스 만들기

---
+++ obstacle.py
@@ -0,0 +1,17 @@
+import play_state
+from pico2d import *
+
+class Obstacle:
+    def __init__(self):
+        self.floor_image = load_image('spike.png')
+        self.x, self.y = 0, 0
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.floor_image.clip_draw(0, 0, 139, 105, self.x - play_state.knight.x + 400, 50)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - play_state.knight.x + 400 - 75, self.y - 50, self.x - play_state.knight.x + 400 + 70, self.y + 50
