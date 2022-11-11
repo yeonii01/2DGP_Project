b0578388e52740457ea651875b657bcda7709e12
yeonii01 <adencho1204@naver.com>
2022-11-12 03:39:53 +0900

    fground 클래스 추가

--- ground.py
+++ ground.py
@@ -18,3 +18,15 @@ class Ground:
 
 
 
+class FGround:
+    def __init__(self):
+        self.floor_image = load_image('fground.png')
+        self.x, self.y = 0, 140
+    def update(self):
+        pass
+    def draw(self):
+        self.floor_image.clip_draw(0, 0, 172, 74, self.x - play_state.knight.x + 400, self.y, 150, 60)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - play_state.knight.x + 400 - 75, self.y - 30, self.x - play_state.knight.x + 400 + 70, self.y + 20
