d8d3b64afa21e5e7b63483c36e4874999a364b68
yeonii01 <adencho1204@naver.com>
2022-12-01 00:57:43 +0900

    대화 말풍선 느낌 추가

--- Npc.py
+++ Npc.py
@@ -10,6 +10,7 @@ class NPC:
         self.dialogue = 0
         self.npc_image = load_image('npc.png')
         self.font = load_font('Cafe24Danjunghae.TTF', 20)
+        self.talkeffect = load_image('lifeup_effect.png')
     def update(self):
         if math.fabs(self.x - play_state.knight.x) <= 200:
             self.talk = True
@@ -22,7 +23,7 @@ class NPC:
                 self.font.draw(self.x- play_state.knight.x + 400, self. y + 75, f'FIND THE KEY',(255,255,255))
             elif self.dialogue == 2:
                 self.font.draw(self.x- play_state.knight.x + 400, self. y + 75, f'THE HINT IS BELL',(255,255,255))
-
+            self.talkeffect.clip_draw(0,0,91,83,self.x- play_state.knight.x + 450, self. y + 85,500,100)
         # draw_rectangle(*self.get_bb())
 
     def get_bb(self):
